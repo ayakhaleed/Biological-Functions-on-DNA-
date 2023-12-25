4092af8120d4baa68463ccf97750234fff5b7d27
Aya Khaled Mohamed <89555718+ayakhaleed@users.noreply.github.com>
2023-12-25 22:49:49 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Variola Genes DB</title>
+  <link rel="stylesheet" href="/server/index.css" type="text/css" />
+</head>
+
+<body>
+  <ul>
+    <li><a href="/server/index.php">Home</a></li>
+    <li class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">Genes</a>
+      <div class="dropdown-content">
+        <a href="/server/db/select/project.html">Genes List</a>
+        <a href="/server/db/create/index.html">Discovered New</a>
+        <a href="/server/db/delete/index.html">Found False Gene</a>
+        <a href="/server/db/update/index.html">Correct Gene</a>
+      </div>
+    </li>
+    <li class="dropdown">
+      <a href="javascript:void(0)" class="dropbtn">Sequences</a>
+      <div class="dropdown-content">
+        <a href="/server/func/gc/index.html">GC</a>
+        <a href="/server/func/transcripe/index.html">Transcripe</a>
+        <a href="/server/func/complement/index.php">Complement</a>
+        <a href="/server/func/revcomp/index.php">Rev. Complement</a>
+        <a href="/server/func/most/index.html">Most Freq. K-mer</a>
+      </div>
+    </li>
+    <li class="nav-title">
+      <h2>Variola Genes</h2>
+    </li>
+  </ul>
+
+  <h1 class="title">Variola Genes</h1>
+  <h3 class="subtitle">DB to save lab records on Variola virus's genes</h3>
+
+  <div class="sections">
+    <div class="text-1">
+      <p>Variola major (smallpox ), acute infectious disease that begins with a high fever, headache, and back pain and then proceeds to an eruption on the skin that leaves the face and limbs covered with cratered pockmarks, or pox.</p>
+    </div>
+    <div class="image-1"><img src="https://thenativeantigencompany.com/wp-content/uploads/2016/07/Pox-Virus-01.jpg" /></div>
+    <div class="image-2"><img src="https://www.news-medical.net/images/news/ImageForNews_722440_16606501375636632.jpg" /></div>
+    <div class="text-2">
+      <p>What is Smallpox? Before smallpox was eradicated, it was a serious infectious disease caused by the variola virus. It was contagious—meaning, it spad from one person to another. People who had smallpox had a fever and a distinctive, progressive skin rash.
+
+        Most people with smallpox recovered, but about 3 out of every 10 people with the disease died. Many smallpox survivors have permanent scars over large areas of their body, especially their faces. Some are left blind.</p>
+    </div>
+    <div class="text-3">
+      <p>Thousands of years ago, variola virus (smallpox virus) emerged and began causing illness and deaths in human populations, with smallpox outbreaks occurring from time to time. Thanks to the success of vaccination, the last natural outbreak of smallpox in the United States occurred in 1949. In 1980, the World Health Assembly declared smallpox eradicated (eliminated), and no cases of naturally occurring smallpox have happened since.</p>
+    </div>
+    <div class="image-3"><img src="https://www.verywellfamily.com/thmb/Zfx0ekikKE84urF-bE_XFg0ljbE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/MarinaDemidiuk-0f561d3557944249bdfb2cee9990c696.jpg" /></div>
+  </div>
+  <p style="margin-top: 100px;">.</p>
+  <footer>
+    <p>&copy; 2023 Variola Labs. All rights reserved.</p>
+  </footer>
+</body>
+
+</html>
