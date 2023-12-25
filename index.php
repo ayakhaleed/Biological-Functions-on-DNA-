38a1bb07fe52d7cfe3eb7dfbd12a66c78b778d78
Aya Khaled Mohamed <89555718+ayakhaleed@users.noreply.github.com>
2023-12-25 23:03:32 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -32,30 +32,54 @@
       <h2>Variola Genes</h2>
     </li>
   </ul>
+  <div class="results">
+    <?php
+    include '../credentials.php';
+    // $servername = "localhost";
+    // $username = "debian-sys-maint";
+    // $password = "0hbwh4xDVCqpV94G";
+    // $dbname = "Variola";
+    // Create connection
+    $conn = new mysqli(
+      $servername,
+      $username,
+      $password,
+      $dbname
+    );
+    // Check connection
+    if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+    } else {
+      echo "Connected successfully<br> ";
+    }
 
-  <h1 class="title">Variola Genes</h1>
-  <h3 class="subtitle">DB to save lab records on Variola virus's genes</h3>
+    if (isset($_POST['submit'])) {
+      $GIdentifier = $_POST['GIdentifier'];
+      $GLocusTag = $_POST['GLocusTag'];
+      $GNote = $_POST['GNote'];
+      $GProduct = $_POST['GProduct'];
+      $ProteinId = $_POST['ProteinId'];
+      $Translation = $_POST['Translation'];
 
-  <div class="sections">
-    <div class="text-1">
-      <p>Variola major (smallpox ), acute infectious disease that begins with a high fever, headache, and back pain and then proceeds to an eruption on the skin that leaves the face and limbs covered with cratered pockmarks, or pox.</p>
-    </div>
-    <div class="image-1"><img src="https://thenativeantigencompany.com/wp-content/uploads/2016/07/Pox-Virus-01.jpg" /></div>
-    <div class="image-2"><img src="https://www.news-medical.net/images/news/ImageForNews_722440_16606501375636632.jpg" /></div>
-    <div class="text-2">
-      <p>What is Smallpox? Before smallpox was eradicated, it was a serious infectious disease caused by the variola virus. It was contagious—meaning, it spad from one person to another. People who had smallpox had a fever and a distinctive, progressive skin rash.
 
-        Most people with smallpox recovered, but about 3 out of every 10 people with the disease died. Many smallpox survivors have permanent scars over large areas of their body, especially their faces. Some are left blind.</p>
-    </div>
-    <div class="text-3">
-      <p>Thousands of years ago, variola virus (smallpox virus) emerged and began causing illness and deaths in human populations, with smallpox outbreaks occurring from time to time. Thanks to the success of vaccination, the last natural outbreak of smallpox in the United States occurred in 1949. In 1980, the World Health Assembly declared smallpox eradicated (eliminated), and no cases of naturally occurring smallpox have happened since.</p>
-    </div>
-    <div class="image-3"><img src="https://www.verywellfamily.com/thmb/Zfx0ekikKE84urF-bE_XFg0ljbE=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/MarinaDemidiuk-0f561d3557944249bdfb2cee9990c696.jpg" /></div>
+      if (!(empty($GLocusTag) && empty($GNote)
+        && empty($GProduct) && empty($ProteinId) && empty($Translation))) {
+        $sql = "UPDATE Gene SET locus_tag='$GLocusTag', note='$GNote',
+    product='$GProduct',protein_id='$ProteinId',translation='$Translation'
+     WHERE identifier ='$GIdentifier'";
+
+
+        if ($conn->query($sql) === TRUE) {
+          echo "New record from input form  Updated  succsessfully <br> ";
+        } else {
+          echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+      }
+    }
+
+    ?>
   </div>
-  <p style="margin-top: 100px;">.</p>
-  <footer>
-    <p>&copy; 2023 Variola Labs. All rights reserved.</p>
-  </footer>
+
 </body>
 
 </html>
